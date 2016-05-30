3956a5c3acd0fea7f748bbbae23e1d676c032667
runnin86 <sml_design@sml-designdeiMac.local>
2016-05-30 18:08:15 +0800

    2016年05月30日18:08:11

---
+++ App/Views/User/login.js
@@ -0,0 +1,102 @@
+'use strict';
+import React, {
+  AppRegistry,
+  Component,
+  StyleSheet,
+  Text,
+  Image,
+  View,
+  TextInput,
+} from'react-native';
+
+class TestInputextends Component {
+  render() {
+    return (
+      <Viewstyle={{backgroundColor:'#f4f4f4',flex:1}}>
+          <Image
+              style={styles.style_image}
+             source={require('./img/app_icon.png')}/>
+          <TextInput
+              style={styles.style_user_input}
+              placeholder='QQ号/手机号/邮箱'
+              numberOfLines={1}
+              autoFocus={true}
+             underlineColorAndroid={'transparent'}
+              textAlign='center'
+          />
+          <View
+             style={{height:1,backgroundColor:'#f4f4f4'}}
+          />
+          <TextInput
+              style={styles.style_pwd_input}
+              placeholder='密码'
+              numberOfLines={1}
+             underlineColorAndroid={'transparent'}
+              secureTextEntry={true}
+              textAlign='center'
+          />
+          <View
+              style={styles.style_view_commit}
+           >
+            <Text style={{color:'#fff'}}>
+               登录
+            </Text>
+
+          </View>
+
+          <Viewstyle={{flex:1,flexDirection:'row',alignItems: 'flex-end',bottom:10}}>
+             <Textstyle={styles.style_view_unlogin}>
+                 无法登录?
+            </Text>
+            <Textstyle={styles.style_view_register}>
+                 新用户
+            </Text>
+          </View>
+      </View>
+    );
+  }
+}
+const styles =StyleSheet.create({
+  style_image:{
+    borderRadius:35,
+    height:70,
+    width:70,
+    marginTop:40,
+    alignSelf:'center',
+  },
+  style_user_input:{
+      backgroundColor:'#fff',
+      marginTop:10,
+      height:35,
+  },
+   style_pwd_input:{
+      backgroundColor:'#fff',
+      height:35,
+  },
+   style_view_commit:{
+      marginTop:15,
+      marginLeft:10,
+      marginRight:10,
+      backgroundColor:'#63B8FF',
+      height:35,
+      borderRadius:5,
+      justifyContent: 'center',
+      alignItems: 'center',
+  },
+  style_view_unlogin:{
+    fontSize:12,
+    color:'#63B8FF',
+    marginLeft:10,
+  },
+  style_view_register:{
+    fontSize:12,
+    color:'#63B8FF',
+    marginRight:10,
+    alignItems:'flex-end',
+    flex:1,
+    flexDirection:'row',
+    textAlign:'right',
+  }
+});
+
+AppRegistry.registerComponent('TestInput',() => TestInput);
