212a7acad69b4ae73c71bce6aad5dd270c883c95
runnin86 <sml_design@sml-designdeiMac.local>
2016-06-02 19:01:04 +0800

    2016年06月02日19:01:00

---
+++ App/Common/MenuItem.js
@@ -0,0 +1,40 @@
+var React = require('react-native');
+var {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableHighlight
+} = React;
+
+module.exports = React.createClass({
+  _performClick:function(){
+    var onClick = this.props.onClick;
+    if(onClick){
+      onClick();
+    }
+  },
+  render: function() {
+    var height = parseInt(this.props.height);
+    var fontSize = parseInt(this.props.fontSize);
+    return (
+      <TouchableHighlight underlayColor="#dad9d7" onPress={this._performClick}>
+        <View style={{flexDirection:'row',alignItems:'center',backgroundColor:'#ffffff',height:height,paddingLeft:20,paddingRight:16}}>
+          <Image style={[styles.iconSize]}
+            source={{uri: this.props.icon}}/>
+          <Text style={{flex:1,color:'#333333',marginLeft:10,fontSize:fontSize}}>{this.props.title}</Text>
+          <Image style={[styles.iconSize]}
+            source={require('image!arrow_right_grey')} />
+        </View>
+      </TouchableHighlight>
+    );
+  },
+});
+
+var styles = StyleSheet.create({
+  iconSize: {
+    height:20,
+    width:20,
+    resizeMode: Image.resizeMode.contain,
+  }
+});
---
+++ App/Views/User/Setting.js
@@ -0,0 +1,83 @@
+import React from 'react-native';
+import Util from '../../Common/Util';
+
+var {
+	StyleSheet,
+	View,
+	Text,
+  LinkingIOS,
+  TouchableHighlight,
+  SwitchIOS,
+} = React;
+
+var Setting = React.createClass({
+  getInitialState: function() {
+    return {
+      user:null,
+      falseSwitchIsOn: false,
+    };
+  },
+  _call:function(){
+    LinkingIOS.openURL('tel://4007008780');
+  },
+  render: function() {
+    return (
+      <View style={{backgroundColor:'#eef0f3',marginTop:68,height:Util.size['height'],}}>
+        <View style={{flexDirection:'row',alignItems:'center',backgroundColor:'#ffffff',height:36,paddingLeft:20,paddingRight:16}}>
+          <Text style={{flex:1,color:'#333333',fontSize:12}}>
+            铃声提醒
+          </Text>
+          <SwitchIOS
+            onValueChange={(value) => this.setState({falseSwitchIsOn: value})}
+            style={{marginBottom: 4}}
+            value={this.state.falseSwitchIsOn} />
+        </View>
+
+        <View style={{flexDirection:'row',alignItems:'center',backgroundColor:'#ffffff',height:36,paddingLeft:20,paddingRight:16}}>
+          <Text style={{flex:1,color:'#333333',fontSize:12}}>
+            WIFI环境下自动更新
+          </Text>
+          <SwitchIOS
+            onValueChange={(value) => this.setState({falseSwitchIsOn: value})}
+            style={{marginBottom: 4}}
+            value={true} />
+        </View>
+
+        <View style={{flexDirection:'row',alignItems:'center',backgroundColor:'#ffffff',height:36,paddingLeft:20,paddingRight:16}}>
+          <Text style={{flex:1,color:'#333333',fontSize:12}}>
+            版本更新1.10
+          </Text>
+          <Text style={{flex:1,color:'gray',fontSize:10,textAlign:'right',}}>
+            已是最新版本
+          </Text>
+        </View>
+
+        <TouchableHighlight style={[styles.btn]} underlayColor='#0057a84a' onPress={this.login}>
+          <Text style={{color:'#fff'}}>退出登录</Text>
+        </TouchableHighlight>
+
+        <TouchableHighlight
+          style={{justifyContent:'center',alignItems:'center',backgroundColor:'#ffffff',height:45,marginTop:30}}
+          underlayColor="#dad9d7" onPress={()=>this._call()}>
+         <Text>拨打客服400-700-8780</Text>
+        </TouchableHighlight>
+
+      </View>
+    );
+  },
+});
+
+var styles = StyleSheet.create({
+  btn:{
+    height:35,
+    margin:10,
+    backgroundColor:'#FF4500',
+    justifyContent:'center',
+    alignItems:'center',
+    borderRadius: 10,
+    borderWidth:1,
+    borderColor:'#ffffff',
+  }
+});
+
+module.exports = Setting;
