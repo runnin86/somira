ecacd4c68a0d76e60bcbfbf806999a81289e25ad
runnin86 <sml_design@sml-designdeiMac.local>
2016-06-04 11:45:25 +0800

    2016年06月04日11:45:19

--- App/Component/ShoppingCart/ItemList.js
+++ App/Component/ShoppingCart/ItemList.js
@@ -25,7 +25,7 @@ module.exports = React.createClass({
       return {
         dataSource: new ListView.DataSource({rowHasChanged: (row1, row2) => row1 !== row2}),
         loaded: false,
-        cateId: 1,
+        cateId: 0,
       };
     },
     //只调用一次，在render之后调用
@@ -70,7 +70,7 @@ module.exports = React.createClass({
             ({code, msg, info})=>{
               // console.log(code);
               // console.log(msg);
-              console.log(info);
+              // console.log(info);
               this.setState({
                 dataSource: this.state.dataSource.cloneWithRows(code === 1?info:''),
                 loaded: true
@@ -105,46 +105,46 @@ module.exports = React.createClass({
             {
               this.state.cateId === 0
               ?
-              <View style={css.container}>
-                <View style={{flexDirection : 'column'}}>
-                  <Image style={[css.cartImg,{width: 42,height: 48,}]}
+              <View style={[css.row,css.container]}>
+                <View style={css.column}>
+                  <Image style={[css.left10,css.bottom10,css.right4,{width: 42,height: 48,}]}
                     source={{uri: item.expertHead}} />
-                  <Text style={{fontWeight : '100',fontSize : 10,alignSelf:'center',marginTop:-6,}}
+                  <Text style={[css.fontWeight,css.fontSize10,{alignSelf:'center',marginTop:-6,}]}
                     numberOfLines={1}>
                     {item.expertName}
                   </Text>
                 </View>
                 <View style={css.flex1}>
-                  <View style={css.cartRow}>
-                    <Text style={{fontWeight: '100',fontSize: 10}}>
+                  <View style={[css.row,css.left10,css.right15,css.bottom4]}>
+                    <Text style={[css.fontWeight,css.fontSize10]}>
                       {item.planName}
                     </Text>
-                    <Text style={{fontWeight: '100',fontSize: 10,flex:1,textAlign:'right',}}>
+                    <Text style={[css.fontWeight,css.fontSize10,css.flex1,css.textRight]}>
                       {item.addTime}
                     </Text>
                   </View>
-                  <View style={css.cartRow}>
+                  <View style={[css.row,css.left10,css.right15,css.bottom4]}>
                     <TextInput
                       keyboardType ='numeric'
                       style={css.input}
                       value={item.amount+''}
                       onChangeText={(text) => this.setState({userId: text})}/>
-                    <Text style={{fontWeight:'100',fontSize:10,marginLeft:10,marginTop:6,}}>
+                    <Text style={[css.fontWeight,css.fontSize10,css.left10,css.top4]}>
                       {item.planAmount}元
                     </Text>
       						</View>
-                  <View style={[css.cartRow,{marginTop:4,}]}>
-                    <View style={{flexDirection: 'row'}}>
-                      <Image style={{width: 12,height: 12,marginRight:4,}}
+                  <View style={[css.row,css.left10,css.right15,css.bottom4,css.top4]}>
+                    <View style={css.row}>
+                      <Image style={[css.right4,{width: 12,height: 12}]}
                         source={require('image!方案详情-收益区')} />
-                      <Text style={{fontWeight:'100',fontSize:10}}>
+                      <Text style={[css.fontWeight,css.fontSize10]}>
                         {item.rangeName}
                       </Text>
                     </View>
-                    <View style={{flexDirection: 'row',right:-40,}}>
-                      <Image style={{width: 9,height: 13,marginRight:4,}}
+                    <View style={[css.row,{right:-40,}]}>
+                      <Image style={[css.right4,{width: 9,height: 13,}]}
                         source={require('image!单价')} />
-                      <Text style={{fontWeight:'100',fontSize:10,flex:1,textAlign:'right',}}>
+                      <Text style={[css.fontWeight,css.fontSize10,css.flex1]}>
                         {item.planAmount}元
                       </Text>
                     </View>
@@ -153,18 +153,33 @@ module.exports = React.createClass({
                 <Image style={css.delBtn} source={require('image!删除')} />
       				</View>
               :
-              <View style={css.container}>
-                <Image style={[css.cartImg,{width: 60,height: 60,}]}
+              <View style={[css.row,css.container]}>
+                <Image style={[css.bottom10,css.right4,{width: 60,height: 60,}]}
                   source={{uri: item.images?item.images.split(',')[0]:''}} />
                 <View style={css.flex1}>
-                  <Text style={{fontWeight : '100',fontSize : 11}} numberOfLines={1}>
+                  <Text style={[css.fontWeight,{fontSize: 11}]} numberOfLines={1}>
                     {item.name}
                   </Text>
-                  <Text style={{fontWeight : '100',fontSize : 8}} numberOfLines={1}>
+                  <Text style={[css.fontWeight,{fontSize: 8}]} numberOfLines={1}>
                     {item.content}
                   </Text>
-
-
+                  <TextInput
+                    keyboardType ='numeric'
+                    style={[css.input,css.row,css.top4]}
+                    value={item.amount+''}
+                    onChangeText={(text) => this.setState({userId: text})}/>
+                  <View style={[css.row,css.top4]}>
+                    <Text style={[css.fontWeight,css.fontSize10]}>
+                      需:
+                      <Text style={css.red}>
+                        {item.amount}
+                      </Text>
+                      元
+                    </Text>
+                    <Text style={[css.fontWeight,css.fontSize10,css.flex1,css.textCenter]}>
+                      总价:{item.totalCount}元
+                    </Text>
+                  </View>
                 </View>
                 <Image style={css.delBtn} source={require('image!删除')} />
               </View>
@@ -191,31 +206,16 @@ var css = StyleSheet.create({
   },
   flex1 : {
     flex : 1,
-    // flexDirection : 'column'
   },
   listView : {
-    // backgroundColor : '#ffffff'
+    backgroundColor : '#eeeeee'
   },
   container: {
-    // flex : 1,
     paddingTop: 8,
-    paddingBottom: 8,
-    flexDirection: 'row',
+    paddingBottom: 4,
     borderBottomColor : '#eeeeee',
     borderBottomWidth : 1,
     backgroundColor: '#ffffff',
-  },
-  cartRow : {
-    flexDirection : 'row',
-    // alignItems: 'center',
-    marginLeft: 10,
-    marginRight: 18,
-    marginBottom : 4,
-  },
-  cartImg : {
-    marginRight: 15,
-    marginBottom: 10,
-    left: 10
   },
   delBtn: {
     right: 10,
@@ -232,8 +232,45 @@ var css = StyleSheet.create({
     // borderColor:'#3164ce',//#b0b0b0,#c40001
     borderColor:'b0b0b0',
     borderWidth:1,
-    // color:'#fff',
-    // flex:1,
     fontSize:14,
   },
+  fontWeight: {
+    fontWeight: '100'
+  },
+  fontSize10: {
+    fontSize: 10
+  },
+  textRight: {
+    textAlign:'right'
+  },
+  textCenter: {
+    textAlign:'center'
+  },
+  red: {
+    color: 'red'
+  },
+  column: {
+    flexDirection: 'column'
+  },
+  row: {
+    flexDirection: 'row'
+  },
+  left10: {
+    marginLeft: 10
+  },
+  right15: {
+    marginRight: 15
+  },
+  right4: {
+    marginRight: 4
+  },
+  bottom4: {
+    marginBottom: 4
+  },
+  bottom10: {
+    marginBottom: 10
+  },
+  top4: {
+    marginTop:4
+  }
 });
