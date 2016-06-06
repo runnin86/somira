07b704840ec77c4f09190f7c359ac5e1b39f4c50
runnin86 <sml_design@sml-designdeiMac.local>
2016-06-06 14:16:34 +0800

    2016年06月06日14:15:53

--- App/Component/HappyPurchase/ItemList.js
+++ App/Component/HappyPurchase/ItemList.js
@@ -2,9 +2,12 @@
 'use strict';
 
 import React from 'react-native';
-import ItemCell from './ItemCell';
+import Button from 'react-native-button';
 import Util from './../../Common/Util';
+import Progress from '../../Common/Progress';
 import * as net from './../../Network/Interface';
+
+import * as Animatable from 'react-native-animatable';
 
 var {
   StyleSheet,
@@ -12,6 +15,7 @@ var {
   Text,
   Image,
   View,
+  TouchableOpacity,
 } = React;
 
 module.exports = React.createClass({
@@ -24,7 +28,7 @@ module.exports = React.createClass({
   },
   //只调用一次，在render之后调用
   componentDidMount() {
-    this.fetchData();
+    this.fetchData(0);
   },
   //render 之前调用
   //之所以取nextProps的值而不直接取this.props.cateId 是因为componentWillReceiveProps的更新早于props的更新
@@ -67,10 +71,52 @@ module.exports = React.createClass({
         style={css.listView}/>
     );
   },
+  _animatables: {},
+  _pushCart: function(event,id){
+    this._animatables[id]['slideInLeft'](1000);
+  },
   //渲染每一行
   renderRow(item) {
+    let progressNum = (parseFloat(item.totalCount) - parseFloat(item.codeCount)) / parseFloat(item.totalCount);
+    let img = item.images.split(',')[0];
     return (
-      <ItemCell item={item} onSelect={this.props.onSelect.bind(this,item)} />
+      <TouchableOpacity onPress={this.props.onSelect.bind(this,item)}>
+        <View style={css.container}>
+          <Animatable.Image ref={component => this._animatables[item.id] = component} style={css.goodImg} source={{uri : img}} />
+          <View style={css.goodInfo}>
+            <Text style={css.goodTit,{fontSize : 14}} numberOfLines={2}>{item.name}</Text>
+            <Text style={css.goodTit,{fontWeight : '100',fontSize : 10,marginTop : 4}} numberOfLines={1}>{item.content}</Text>
+            {/*
+            <View style={css.goodRow}>
+              <Text style={css.redPrice}><Text style={css.yen}>&yen;</Text> {item.orderprice || item.codeCount}</Text>
+              <Text style={css.whitePrice}>&yen;{item.totalCount}</Text>
+            </View>
+            */}
+            <View style={css.progress}>
+              <Progress progress={progressNum}/>
+            </View>
+            <View style={css.goodRow}>
+              <View>
+                <Text style={css.redPrice}>
+                  {item.totalCount}
+                </Text>
+                <Text style={css.goodTit,{fontWeight:'100',fontSize:10}}>总需</Text>
+              </View>
+              <View>
+                <Text style={css.whitePrice}>
+                  {item.codeCount}
+                </Text>
+                <Text style={css.goodTit,{fontWeight:'100',fontSize:10}}>剩余</Text>
+              </View>
+              <View style={css.goodBtnWarp}>
+                <Button onPress={(name)=>this._pushCart(this,item.id)}>
+                  <Image style={css.goodBtnImg} source={{uri: 'icon_addcar_big_black'}}/>
+                </Button>
+              </View>
+            </View>
+          </View>
+        </View>
+      </TouchableOpacity>
     );
   },
 	render() {
@@ -92,5 +138,70 @@ var css = StyleSheet.create({
   },
   listView : {
     // backgroundColor : '#ffffff'
+  },
+  container: {
+    flex : 1,
+    paddingTop: 10,
+    paddingBottom: 2,
+    flexDirection: 'row',
+    borderBottomColor : '#eeeeee',
+    borderBottomWidth : 1,
+    backgroundColor: '#ffffff',
+  },
+  //左侧商品图
+  goodImg : {
+    width : 60,
+    height : 60,
+    marginRight: 15,
+    left: 10
+  },
+  //右侧商品信息
+  goodInfo : {
+    flex : 1,
+    flexDirection : 'column'
+  },
+  goodTit : {
+    // height : 16,
+    color : '#000000',
+    textAlign: 'left',
+    // marginRight: 2,
+  },
+  // 价格
+  goodRow : {
+    flexDirection : 'row',
+    alignItems: 'center',
+    marginBottom : 10
+  },
+  redPrice : {
+    color : '#c40001',
+    width: (Util.size['width']-70)/2
+  },
+  whitePrice : {
+    fontSize : 12,
+    color : '#b0b0b0'
+  },
+  // 购买及按钮
+  goodExtra : {
+    flexDirection :'row',
+    alignItems: 'center',
+    justifyContent : 'space-between'
+  },
+  goodSold : {
+    color : '#b0b0b0'
+  },
+  goodBtnWarp : {
+    position : 'absolute',
+    right : 20,
+    top : -4
+  },
+  goodBtnImg: {
+    width: 36,
+    height: 36,
+    borderRadius: 18
+  },
+  progress: {
+    width: (Util.size['width']-158),
+    marginTop: 5,
+    marginBottom: 5
   }
 });
