59a56ceed5c1cdb897478128ef615c5741cd1a92
runnin86 <sml_design@sml-designdeiMac.local>
2016-06-08 18:43:52 +0800

    2016年06月08日18:43:49

--- index.ios.js
+++ index.ios.js
@@ -9,6 +9,7 @@ import {
   NavigatorIOS
 } from 'react-native';
 
+import Store from 'react-native-simple-store';
 import HappyPurchase from './App/Views/Purchase/HappyPurchase';
 import Plan from './App/Views/Plan/Plan';
 import UserCenter from './App/Views/User/UserCenter';
@@ -21,9 +22,10 @@ StatusBarIOS.setHidden(false);
 var somira = React.createClass({
   getInitialState(){
     return {
-      selectedTab: 'plan',
+      selectedTab: 'hp',
       notifyCartCount: 18,
-      notifyUserCount: 1
+      notifyUserCount: 1,
+      showPlan: false,
     };
   },
   changeTab(tabName){
@@ -31,25 +33,44 @@ var somira = React.createClass({
       selectedTab : tabName
     });
   },
+  isShowPlan() {
+    // 根据用户类型判断是否展示方案
+    Store.get('user').then((user)=>{
+      if (user && user.user_type === 1) {
+        console.log('展示方案');
+        this.state.showPlan = true;
+      }
+      else {
+        console.log('不展示方案。。。。');
+        this.state.showPlan = false;
+      }
+    });
+  },
   render: function() {
     // #26292E;#292C33;
     return (
       <TabBarIOS tintColor="#B22222" barTintColor="#FFF5EE">
-        <TabBarIOS.Item
-          title="购买方案" icon={{uri:'购买方案',scale:2,isStatic:true}}
-          selected={this.state.selectedTab === 'plan'}
-          onPress={()=>{this.changeTab('plan')}}>
-          <NavigatorIOS style={css.container}
-            barTintColor={Util.headerColor}
-            tintColor={'#333344'}
-            navigationBarHidden={false}
-            initialRoute={{
-              title: '购买方案',
-              titleTextColor: Util.headerTitleColor,
-              component: Plan,
-              wrapperStyle: css.wrapperStyle
-            }}/>
-        </TabBarIOS.Item>
+        {
+          this.state.showPlan
+          ?
+          <TabBarIOS.Item
+            title="购买方案" icon={{uri:'购买方案',scale:2,isStatic:true}}
+            selected={this.state.selectedTab === 'plan'}
+            onPress={()=>{this.changeTab('plan')}}>
+            <NavigatorIOS style={css.container}
+              barTintColor={Util.headerColor}
+              tintColor={'#333344'}
+              navigationBarHidden={false}
+              initialRoute={{
+                title: '购买方案',
+                titleTextColor: Util.headerTitleColor,
+                component: Plan,
+                wrapperStyle: css.wrapperStyle
+              }}/>
+          </TabBarIOS.Item>
+          :
+          null
+        }
         <TabBarIOS.Item
           title="乐夺宝" icon={{uri:'乐夺宝',scale:3,isStatic:true}}
           selected={this.state.selectedTab === 'hp'}
