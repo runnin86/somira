99dd80d706f36d50e52ba59db3afd4e8fd4f6936
runnin86 <sml_design@sml-designdeiMac.local>
2016-06-22 18:10:41 +0800

    2016年06月22日18:10:34

--- App/Network/Interface.js
+++ App/Network/Interface.js
@@ -1,6 +1,6 @@
 // const URL_COMMON = 'http://api.zqsml.com/api/v1/sml/'
 const URL_COMMON = 'http://123.57.217.199:9587/api/v1/sml/'
-
+// 15939306231
 // 方案接口地址
 export let planApi = {
   // banner地址
