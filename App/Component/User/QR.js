147bd2b6fc8a839806043616f2a3bd059acb472e
runnin86 <dda1314@163.com>
2016-08-13 20:11:55 +0800

    2016年08月13日20:11:52

--- App/Component/User/QR.js
+++ App/Component/User/QR.js
@@ -10,6 +10,7 @@ var {
   StyleSheet,
   Text,
   View,
+  Image,
 } = React;
 
 module.exports = React.createClass({
@@ -17,6 +18,11 @@ module.exports = React.createClass({
    * 获取实例初始状态(state，来自props)
    */
   getInitialState: function () {
+    Store.get('user').then((userdata)=>{
+      this.setState({
+        uname:userdata.user_name,
+      })
+    });
     return {
       text: ''
     }
@@ -44,11 +50,24 @@ module.exports = React.createClass({
   render: function() {
     return (
       <View style={css.container}>
-        <QRCode
-          value={this.state.text}
-          size={Util.size['width']-100}
-          bgColor='#FFFFFF'
-          fgColor='#000000'/>
+        <Image style={[css.backgroundImg,css.center]} source={{uri: 'qrbackground'}}>
+          <View style={[css.center,{marginTop: -(Util.size['height']*0.292)}]}>
+            <QRCode
+              value={this.state.text}
+              size={Util.size['width']*0.44}
+              bgColor='#FFFFFF'
+              fgColor='#000000'/>
+          </View>
+          <View style={css.textArea}>
+            <Text style={css.textDesc}>
+              您现在要扫描用户[
+                <Text style={{color:'#8A2BE2'}}>
+                  {this.state.uname}
+                </Text>
+              ]的二维码
+            </Text>
+          </View>
+        </Image>
       </View>
     );
   }
@@ -57,8 +76,31 @@ module.exports = React.createClass({
 var css = StyleSheet.create({
   container: {
     flex: 1,
+    // backgroundColor: '#f4f4f4',
+    marginTop: 26,
+  },
+  center: {
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#f4f4f4',
   },
+  backgroundImg: {
+    height: Util.size['height'],
+    width: Util.size['width'],
+    resizeMode: Image.resizeMode.stretch,
+  },
+  textArea: {
+    flexDirection:'row',
+    alignItems:'center',
+    backgroundColor:'#ffffff',
+    height:30,
+    width:Util.size['width'],
+    top:Util.size['height']*0.04,
+  },
+  textDesc: {
+    flex:1,
+    color:'#333333',
+    fontSize:13,
+    fontWeight:'100',
+    textAlign: 'center'
+  }
 });
